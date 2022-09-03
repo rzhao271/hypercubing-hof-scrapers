701eeb940cea626ccb3f83e4ea1d455c8d0a3fd6
Raymond Zhao <7199958+rzhao271@users.noreply.github.com>
2022-09-02 18:04:03 -0700

    Add sh script to download pages

---
+++ scrape-pages.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+page_dir="pages"
+wget -O "${page_dir}/mc4d-hof.html" https://superliminal.com/cube/halloffame.htm
+wget -O "${page_dir}/mc5d-hof.html" http://www.gravitation3d.com/magiccube5d/hallofinsanity.html
+wget -O "${page_dir}/m120c-hof.html" http://www.gravitation3d.com/magic120cell/index.html
+wget -O "${page_dir}/mc7d-hof.html" https://superliminal.com/andrey/mc7d/
+wget -O "${page_dir}/mht633-hof.html" https://superliminal.com/andrey/mht633/
