2c04dedad5332b2b915a26c08c23101e33ec2a7d
Raymond Zhao <7199958+rzhao271@users.noreply.github.com>
2022-09-04 13:12:55 -0700

    Learn even more about solve orders

--- analyzers/solve_orders.py
+++ analyzers/solve_orders.py
@@ -1,15 +1,21 @@
 from os import path
 
 import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
 
 script_dir = path.dirname(path.realpath(__file__))
 in_file = path.join(script_dir, '../tables/combined.csv')
-solve_orders_by_solver_file = path.join(script_dir,
+solve_order_by_solver_file = path.join(script_dir,
     '../tables/solve_order_by_solver.csv')
+solve_order_by_solver_uncommon_file = path.join(script_dir,
+    '../tables/solve_order_by_solver_uncommon.csv')
 solve_order_frequencies_file = path.join(script_dir,
     '../tables/solve_order_frequencies.csv')
-solve_order_frequencies_plot_file = path.join(script_dir, '../figures/top_solve_orders.png')
+solve_order_frequencies_plot_file = path.join(script_dir,
+    '../figures/top_solve_orders.png')
+solve_order_durations_plot_file = path.join(script_dir,
+    '../figures/solve_order_durations.png')
 
 headers = ['puzzle_name', 'solve_num', 'solver', 'solve_epoch']
 df = pd.read_csv(in_file, names=headers)
@@ -18,41 +24,49 @@ df = pd.read_csv(in_file, names=headers)
 groups = df.groupby(by='solver')
 
 # We first want solve orders by solver
-solve_orders_by_solver_headers = ['solver', 'solve_order', 'total_duration']
-solve_orders_by_solver_records = []
+solve_order_by_solver_headers = ['solver', 'solve_order', 'total_duration']
+solve_order_by_solver_records = []
 for solver, group in groups:
     group = group.sort_values(by='solve_epoch')
     solve_order = '->'.join(group['puzzle_name'].tolist())
     epoch_list = group['solve_epoch'].tolist()
     total_duration = float(epoch_list[-1]) - float(epoch_list[0])
-    solve_orders_by_solver_records.append((solver, solve_order, total_duration))
+    solve_order_by_solver_records.append((solver, solve_order, total_duration))
 
-solve_order_df = pd.DataFrame.from_records(solve_orders_by_solver_records,
-    columns=solve_orders_by_solver_headers)
+solve_order_df = pd.DataFrame.from_records(solve_order_by_solver_records,
+    columns=solve_order_by_solver_headers)
 
 # Include the header this time for clarity
 # Save to a CSV
-solve_order_df.to_csv(solve_orders_by_solver_file, index=False)
+solve_order_df.to_csv(solve_order_by_solver_file, index=False)
 
-# Now we want the solve_order_frequencies
-# TODO: Refactor this section so we can later plot
-# a histogram of durations
-# solve_order_frequencies_df = solve_order_df
-solve_order_df['count'] = (solve_order_df
+# By uncommon we mean that we want to see solve orders
+# where the solver never solved the 3^4, but we also kick out
+# solvers who only solved the physical 2^4 or only the 3^5
+solve_order_df_uncommon = solve_order_df.loc[
+    (~solve_order_df['solve_order'].str.contains('3\^4'))
+    & (~solve_order_df['solve_order'].str.fullmatch('Physical 2\^4'))
+    & (~solve_order_df['solve_order'].str.fullmatch('3\^5'))]
+solve_order_df_uncommon.to_csv(solve_order_by_solver_uncommon_file, index=False)
+
+solve_order_frequencies_df = solve_order_df
+solve_order_frequencies_df['count'] = (solve_order_frequencies_df
     .groupby(by='solve_order')['solve_order']
     .transform('count'))
-solve_order_df = (solve_order_df[['solve_order', 'count']]
+solve_order_frequencies_df = (solve_order_frequencies_df
+    [['solve_order', 'count']]
     .drop_duplicates()
     .sort_values(by=['count', 'solve_order'], ascending=[False, True]))
 
 # Save to another CSV
-solve_order_df.to_csv(solve_order_frequencies_file,
+solve_order_frequencies_df.to_csv(solve_order_frequencies_file,
     columns=['solve_order', 'count'], index=False)
 
 # Reverse the data for the bar chart
-solve_order_df = solve_order_df.loc[solve_order_df['count'] > 1].loc[::-1]
-solve_orders = solve_order_df['solve_order'].tolist()
-counts = solve_order_df['count'].tolist()
+solve_order_frequencies_df = (solve_order_frequencies_df
+    .loc[solve_order_frequencies_df['count'] > 1].loc[::-1])
+solve_orders = solve_order_frequencies_df['solve_order'].tolist()
+counts = solve_order_frequencies_df['count'].tolist()
 
 # Create a bar chart
 fig, ax = plt.subplots(figsize=(12, 10))
@@ -63,4 +77,23 @@ ax.set_ylabel('Solve order', fontsize=16)
 ax.set_title('Most frequent solve orders', fontsize=20)
 fig.tight_layout()
 fig.savefig(solve_order_frequencies_plot_file)
+
+def process_duration(d):
+    # Convert seconds to years
+    return d / (60 * 60 * 24 * 365.25)
+
+solve_order_durations = [process_duration(d) for d in
+    solve_order_df['total_duration'].tolist() if d > 0]
+
+# Create a histogram for solve order durations
+ax.clear()
+ax.hist(solve_order_durations, bins=np.arange(21))
+ax.set_xlabel('Solve order duration (years)', fontsize=16)
+ax.set_ylabel('Number of non-singular solve orders of that duration',
+    fontsize=16)
+ax.set_title('Non-singular solve order durations', fontsize=20)
+ax.set_xticks(np.arange(21))
+fig.tight_layout()
+fig.savefig(solve_order_durations_plot_file)
+
 plt.close(fig)
